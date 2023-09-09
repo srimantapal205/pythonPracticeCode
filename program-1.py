aeb0adcc468fd7921fd610f464db1310ce42e90d
Srimanta Pal <srimantapal205@hotmail.com>
2023-09-09 20:25:36 +0530

    first commit

---
+++ program-1.py
@@ -0,0 +1,42 @@
+#Fundamental Data Type
+
+# int 
+# float
+# bool
+# str
+# list
+# tuple
+# set
+# dict
+#Class  Costom Type
+
+#Specialized Data Type
+
+# print(2+6)
+# print(2-6)
+# print(2*6)
+# print(2/6)
+
+# print(type(2+6))
+# print(type(2-6))
+# print(type(2*6))
+# print(type(2/6))
+
+# print(2 ** 3) #Power of number
+# print(5 // 4) # Division with int value
+# print(5 % 4) #Modulo
+
+# Math Functions
+
+# print(round(3.1))
+# print(round(3.9))
+# print(abs(-30))
+
+# Operato Precedence
+
+# print((10*10) + 2**2)
+
+# print(bin(5))
+# print(int('0b101',2))
+
+# 

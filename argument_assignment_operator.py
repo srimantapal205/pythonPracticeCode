bc8acee8303f998bd8f6d75762f94782cce296cc
Srimanta Pal <srimantapal205@hotmail.com>
2023-09-09 22:44:27 +0530

    Type Conversion & Escape Sequence

---
+++ argument_assignment_operator.py
@@ -0,0 +1,17 @@
+# Augmented Assignment Operator
+
+some_value = 5
+some_value += 2
+print(some_value)
+
+#Type Conversion
+
+print(str(100))
+
+#Escape Sequence
+
+weather =  "It's Sunny"
+
+sent = "It\'s \"Kind of \" Sunny"
+
+print(sent)
--- program-1.py
+++ program-1.py
@@ -12,31 +12,58 @@
 
 #Specialized Data Type
 
-# print(2+6)
-# print(2-6)
-# print(2*6)
-# print(2/6)
+print(2+6)
+print(2-6)
+print(2*6)
+print(2/6)
 
-# print(type(2+6))
-# print(type(2-6))
-# print(type(2*6))
-# print(type(2/6))
+print(type(2+6))
+print(type(2-6))
+print(type(2*6))
+print(type(2/6))
 
-# print(2 ** 3) #Power of number
-# print(5 // 4) # Division with int value
-# print(5 % 4) #Modulo
+print(2 ** 3) #Power of number
+print(5 // 4) # Division with int value
+print(5 % 4) #Modulo
 
 # Math Functions
 
-# print(round(3.1))
-# print(round(3.9))
-# print(abs(-30))
+print(round(3.1))
+print(round(3.9))
+print(abs(-30))
 
 # Operato Precedence
 
-# print((10*10) + 2**2)
+print((10*10) + 2**2)
 
-# print(bin(5))
-# print(int('0b101',2))
+print(bin(5))
+print(int('0b101',2))
 
-# 
+# Varible in diffrent case
+test_case = 1,
+_test_case = 2
+testCase = 3
+
+# constants
+PI =3.14
+
+#String
+
+print(type('Hi'))
+username = 'superuser'
+password = 'supuruser1234'
+
+long_string = '''
+    WOW
+    0 0
+    ---
+'''
+print(long_string)
+
+str1 = 'Hello'
+str2 = 'World'
+
+fullString = str1 + ' ' + str2
+
+print(fullString)
+

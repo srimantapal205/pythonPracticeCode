e78d739e227ae41e878ea6fc0b42297aa230f9ca
Srimanta Pal <srimantapal205@hotmail.com>
2023-09-11 10:03:35 +0530

    Stirng index  array diffrent from

--- argument_assignment_operator.py
+++ argument_assignment_operator.py
@@ -15,3 +15,35 @@ weather =  "It's Sunny"
 sent = "It\'s \"Kind of \" Sunny"
 
 print(sent)
+
+#formated string
+name = "Johnny"
+age = 55
+
+print(f'Hi {name}, You are {age} years old')
+
+#Stirng index
+text = 'Hi Hello'
+print(text[0])
+print(text[7])
+
+text2 = '0123456789'
+      #  0123456789
+      
+#[start:stop:stepover]
+print(text2)
+print(text2[0:10])
+print(text2[0:10:2])#bydefault stepover value is 1
+print(text2[1:])
+print('arr:'+ text2[0:0])
+print('arr:'+ text2[:])
+print('arr:'+ text2[:5])
+print('arr:'+ text2[::1])
+print('arr:'+ text2[::2])
+print('arr:'+ text2[:2:])
+print('arr:'+ text2[-1]) #Last index value of array
+print('arr:'+ text2[::-1]) #Revers order of array step 1
+print('arr:'+ text2[::-2]) #Revers order of array step 2
+
+
+

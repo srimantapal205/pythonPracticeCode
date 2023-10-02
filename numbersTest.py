9eb47ec9c2cf0f1387660e58ac66fa75d5001fe1
Srimanta Pal <srimantapal205@hotmail.com>
2023-10-02 22:00:18 +0530

    Numbers type file

---
+++ numbersTest.py
@@ -0,0 +1,20 @@
+intNumber = 50
+floatNumber = 5.5
+complexNumber = 5j
+
+#convert from int to float:
+a = float(intNumber)
+
+#convert from float to int:
+b = int(floatNumber)
+
+#convert from int to complex:
+c = complex(intNumber)
+
+print(a)
+print(b)
+print(c)
+
+print(type(a))
+print(type(b))
+print(type(c))

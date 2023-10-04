b7d275035c76363f7f74f8e89ed5852bb8f2eeae
Srimanta Pal <srimantapal205@hotmail.com>
2023-10-04 23:14:16 +0530

    Access Tuple Items

---
+++ tuples.py
@@ -0,0 +1,17 @@
+tupleValue = (1,2,3,4,5,6)
+
+print(type(tupleValue))
+print(tupleValue)
+
+print(len(tupleValue))
+
+#Access tuple
+
+stringTuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango" )
+
+print(stringTuple[1])
+print(stringTuple[-1])
+
+print(stringTuple[1:4])
+print(stringTuple[:4])
+

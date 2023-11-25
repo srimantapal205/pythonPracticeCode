1ca33cd89f97a74f954c5c1c8ba49e28a95094d6
Srimanta Pal <srimantapal205@hotmail.com>
2023-11-25 23:59:14 +0530

    loop concept

---
+++ ControlStatement/program_9.py
@@ -0,0 +1,10 @@
+#To display input range using while loop
+
+x = int(input("Enter starting value: "))
+y = int(input("Enter ending value: "))
+
+while x < y:
+    print(x)
+    x+=1
+    #x+=x
+print("end")

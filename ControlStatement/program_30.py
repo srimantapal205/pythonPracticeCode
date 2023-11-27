715dd5ee4229bc1c00d2546cd81b917cd8705a5c
Srimanta Pal <srimantapal205@hotmail.com>
2023-11-27 09:58:42 +0530

    start using def for function

---
+++ ControlStatement/program_30.py
@@ -0,0 +1,7 @@
+#Function to find sum of two number
+def sum(a,b):
+    print('Sum = ', a+b)
+
+sum(10,20) #call the function sum and pass 10 and 20
+sum(1.6,2.5) #call the function and pass 1.6, 2.5
+
---
+++ ControlStatement/program_31.py
@@ -0,0 +1,9 @@
+#A function to return sum of two number
+def sum(x,y):
+    return x+y #Result is return from here
+
+#call the function sum pass 5 and 10
+#Get the return result into res
+res = sum(5, 10)
+print('Sum : ', res)
+

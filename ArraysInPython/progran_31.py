f1b9801aef69b255c724bfa7662e38259aef74f8
Srimanta Pal <srimantapal205@hotmail.com>
2023-12-06 23:20:30 +0530

    create a view()

---
+++ ArraysInPython/progran_31.py
@@ -0,0 +1,14 @@
+#A python program to create a view() of an exiting array
+
+from numpy import *
+a = arange(1, 6) #Create a with element 1 to 5
+b = a.view() #Create a view of a and call it b
+
+print("Original Array :: ", a)
+print("New Array :: ", b)
+
+b[0] = 99
+print("\nModify 0th element of b\n")
+print("Original Array :: ", a)
+print("New Array :: ", b)
+

44df970a709b3981c123fe281814e5ef5d0e9e8e
Srimanta Pal <srimantapal205@hotmail.com>
2023-12-09 13:36:49 +0530

    Removing space  from a string

--- StringsAndCharacters/CommonCode.py
+++ StringsAndCharacters/CommonCode.py
@@ -1,5 +1,5 @@
 cmnStr = "Core Python"
-
+'''
 for i in cmnStr:print(i, end=" ")
 print('\n')
 
@@ -48,3 +48,37 @@ spc = " "
 str2 = " Python"
 
 print(str1+spc+str2)
+'''
+#Comparing Strings
+s1 = "Box"
+s2 = "Boy"
+if(s1==s2):
+    print("Both are same")
+else:
+    print("Not same")
+    
+if s1<s2:
+  print("S1 less then s2")
+else:
+    print('S1 greater than or equal to s2')
+    
+#Removing space  from a string
+spaceString = "  This is core python   "
+
+x = len(spaceString)
+print(x)
+print("Original String :: ", spaceString)
+
+#Remove space from right side
+rrs= spaceString.rstrip()
+print("Revove Space from right side :: ", rrs, " Length :: ", len(rrs))
+
+#Remove space from left side
+rls= spaceString.lstrip()
+print("Revove Space from left side :: ", rrs, " Length :: ", len(rls))
+
+#Remove space from both side
+ras = spaceString.strip()
+print("Revove Space from left side :: ", ras, " Length :: ", len(ras))
+
+

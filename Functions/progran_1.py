4a504251a97dc4db4cf41aab2993869af6b903fd
Srimanta Pal <srimantapal205@hotmail.com>
2023-12-12 10:19:22 +0530

    Basic sum function

---
+++ Functions/progran_1.py
@@ -0,0 +1,9 @@
+#A function to add two numbers
+def sum(a, b):
+    '''This function finds sum of two numbers'''
+    c = a+b
+    print("Sum :: ", c)
+
+#Call the function
+sum(10,20)
+sum(10.50,20.26)

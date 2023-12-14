608057cff2fbd8be9592a27bedd922ba4077176e
Srimanta Pal <srimantapal205@hotmail.com>
2023-12-14 22:58:58 +0530

    Assign function to varible

---
+++ Functions/progran_9.py
@@ -0,0 +1,8 @@
+#A python program to see how to assign a function to varible
+#assign a function to varible 
+def display(str):
+    return 'Hai '+str
+
+#Assign function to varible
+x = display('Krishna')
+print(x)

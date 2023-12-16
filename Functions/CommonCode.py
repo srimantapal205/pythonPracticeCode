c552b622ba7b92f4eead1b15a4b5304548f6d3f9
Srimanta Pal <srimantapal205@hotmail.com>
2023-12-16 10:43:43 +0530

    formal argument and actual arguments

--- Functions/CommonCode.py
+++ Functions/CommonCode.py
@@ -1,3 +1,14 @@
 x = 10
 ma = id(x)
 print(ma)
+
+# Formal and Actual argument
+def sum(a, b): # a, b are formal argument
+    c= a+b
+    print(c)
+    
+#call the function
+x = 10
+y = 20
+
+sum(x, y) # x, y are actual arguments

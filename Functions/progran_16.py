ea421e147760e5e7bbff36ee6c87b4a9fcdaba91
Srimanta Pal <srimantapal205@hotmail.com>
2023-12-16 10:47:58 +0530

    the positional arguments of a function

---
+++ Functions/progran_16.py
@@ -0,0 +1,7 @@
+#A python program to understan the positional arguments of a function
+def attach(s1, s2):
+    s3 = s1+s2 
+    print('Total stirng :: ', s3)
+
+#call attach() and pass 2 string
+attach('New', 'York')

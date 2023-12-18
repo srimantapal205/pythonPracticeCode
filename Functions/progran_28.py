d5db9c7bc0319d625f037da67e726495dc911f03
Srimanta Pal <srimantapal205@hotmail.com>
2023-12-18 10:05:45 +0530

    use lambda function to calculate squre value

---
+++ Functions/progran_28.py
@@ -0,0 +1,5 @@
+#A python program to create  a lambda function that returns a squre value of given number
+#a lambda function to calculate squre value
+f = lambda x : x*x #lambda function
+value = f(5)
+print('the value of squre is :: ', value)

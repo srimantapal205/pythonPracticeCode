457a782818664ebeb3e7552ed98ecbed58b20961
Srimanta Pal <srimantapal205@hotmail.com>
2023-12-18 10:10:31 +0530

    calculate the sum of two numbers  using lambda function

---
+++ Functions/progran_29.py
@@ -0,0 +1,4 @@
+#A python program to calculate the sum of two numbers
+f = lambda x, y : x+y #Write lambda function
+result = f(5.21, 4.79) #call lambda function
+print('The sum of two value is : ', result) #display the result

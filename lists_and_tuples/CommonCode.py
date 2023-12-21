21301d793f03ecc903582183a509f8ddd7be0853
Srimanta Pal <srimantapal205@hotmail.com>
2023-12-22 00:11:37 +0530

    Aliasing and cloning lists

--- lists_and_tuples/CommonCode.py
+++ lists_and_tuples/CommonCode.py
@@ -1,10 +1,31 @@
+#Connection of two lists
 x = [10,20,30,40,50]
 y = [100,110,120]
 neLst = x+y
 print(neLst)
 
+#Repetition of list
 nlst = [10,20,30,40,50,60,70,80,90,100]
 print(nlst * 2)
 
+#Membership in list
 a = 110
 print(a in nlst) #check if a is member of x
+
+#Aliasing and cloning lists
+m = [10,20,30,40,50]
+n=m # m is aliased as y
+print(m) #will display [10,20,30,40,50]
+print(n) #will display [10,20,30,40,50]
+m[1] = 99 #modify 1st element in x
+print(m) #will display [10,99,30,40,50]
+print(n) #will display [10,99,30,40,50]
+
+a = [10,20,30,40,50]
+b=a[:] # x is cloned as y
+print(a) #will display [10,20,30,40,50]
+print(b) #will display [10,20,30,40,50]
+a[1]=99
+print(a) #will display [10,99,30,40,50]
+print(b) #will display [10,99,30,40,50]
+

e8a001c5e7102b7db2661efeabc716f30fc8a11c
Srimanta Pal <srimantapal205@hotmail.com>
2023-12-21 22:57:39 +0530

    general way to create a list

---
+++ lists_and_tuples/program_1.py
@@ -0,0 +1,16 @@
+#A python program to create lists with diffrents type of element
+#a general way to create a list
+#Create a list with integer number
+num =[10,20,30,40,50,60]
+print('Total list == :: ', num) #display total list
+print('First :: %d, last :: %d' %(num[0], num[5])) #display first and last element
+
+#Create a list with string
+names = ['Raju', 'Vani', 'Gopal', 'Laxmi']
+print('Total List == :: ', names)
+print('First == %s, Last == %s' %(names[0], names[3])) #Display first and las element
+
+#Create a list with diffrent element
+de = [10,20,33.50,24.56,'Ganesh','Laxmi']
+print('Total list == :: ', de)
+print('First == :: %d, Last == :: %s' %(de[0], de[5])) #Display first and last element.

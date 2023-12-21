1d7639477f3e917636eb8c734ad62ede83360b4e
Srimanta Pal <srimantapal205@hotmail.com>
2023-12-21 23:08:45 +0530

    Create  lists using range() function

---
+++ lists_and_tuples/program_2.py
@@ -0,0 +1,15 @@
+#A python program to create list using range() function
+#Create a list using range() function
+#Create a list with 0 to 9 consecutive integer number
+lst1 = range(10)
+for i in lst1:print(i, ',', end=' ') #Display element by element
+print()#throw cursor to next line
+
+#Create a list with integer from 5 to 9
+lst2 = range(5, 9)
+for i in lst2 : print(i, ', ', end=' ')
+print()
+
+#Create a list with odd numbers from 5 to 9
+lst3 = range(5,10,2)
+for i in lst3 : print(i, ', ', end=' ')

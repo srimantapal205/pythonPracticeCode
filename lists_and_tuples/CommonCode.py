e18afc76b4ab9353b4e61e37a7bc8f10d23ca11a
Srimanta Pal <srimantapal205@hotmail.com>
2023-12-21 23:49:14 +0530

    Connection of two list and repetition of list

---
+++ lists_and_tuples/CommonCode.py
@@ -0,0 +1,10 @@
+x = [10,20,30,40,50]
+y = [100,110,120]
+neLst = x+y
+print(neLst)
+
+nlst = [10,20,30,40,50,60,70,80,90,100]
+print(nlst * 2)
+
+a = 110
+print(a in nlst) #check if a is member of x

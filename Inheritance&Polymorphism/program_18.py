e400bba156847364c7e02edb834cbdd0708ebd20
Srimanta Pal <srimantapal205@hotmail.com>
2024-01-08 07:45:52 +0530

    addition operator to act on diffrent type of object

---
+++ Inheritance&Polymorphism/program_18.py
@@ -0,0 +1,14 @@
+#Apython program to use addition operator to act on diffrent type of object
+#Overloading the + operator
+#using + on integers to add them 
+print(10+20)
+
+#using + on string to concatenatr them 
+s1 = "Red"
+s2 ="Fort"
+print(s1+s2)
+
+#using + on liest to make a single list
+l1 =[10,20,30,40]
+l2 =[50,60,70,80,90]
+print(l1 + l2)

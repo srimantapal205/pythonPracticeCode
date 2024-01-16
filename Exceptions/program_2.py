691d491712c0bea8d39035365fb2eb81516361d3
Srimanta Pal <srimantapal205@hotmail.com>
2024-01-16 09:36:22 +0530

    compile-time error 2nd program

---
+++ Exceptions/program_2.py
@@ -0,0 +1,7 @@
+#A python program to demeostrate compile time error
+#another compile-time error
+
+x =10
+if x%2==0:
+    print(x,' is divisible by 2')
+        print('x, 'Is even number'') #IndentationError: unexpected indent

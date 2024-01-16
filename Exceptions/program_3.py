ac9b56bd2e5adb9277f64824d2a7e9757046379e
Srimanta Pal <srimantapal205@hotmail.com>
2024-01-16 09:40:01 +0530

    program to understand runtime errors

---
+++ Exceptions/program_3.py
@@ -0,0 +1,6 @@
+#A python program to understand runtime errors
+def concat(a, b):
+    print(a+b)
+
+#callconcat() and pass arguments
+concat('Hai', 25 ) #TypeError: can only concatenate str (not "int") to str

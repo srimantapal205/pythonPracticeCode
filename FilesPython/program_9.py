27c173151f7f7c094fda504d664bcd92868ed065
Srimanta Pal <srimantapal205@hotmail.com>
2024-02-27 05:10:24 +0530

    #With statement to open a file.

---
+++ FilesPython/program_9.py
@@ -0,0 +1,7 @@
+#A python program to use with to open a file and write some string into the file.bytes
+#With statement to open a file.
+with open('sample.txt', 'w') as f:
+    f.write('I am learner. \n')
+    f.write('Python is attractive languge \n')
+
+ 

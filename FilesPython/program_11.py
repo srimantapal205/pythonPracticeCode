4395af5a774b170eb99d9331b54a98786ea5b6b5
Srimanta Pal <srimantapal205@hotmail.com>
2024-02-27 05:54:03 +0530

    #Create Emp class . #pickle - store Emp class object into emp.dat file.

---
+++ FilesPython/program_11.py
@@ -0,0 +1,11 @@
+#A python program to create an Emp class with employee details as instance varible.
+#Emp class 
+class Emp:
+    def __init__(self, id, name, sal):
+        self.id = id
+        self.name = name
+        self.sal = sal
+        
+    def display(self):
+        print("{:5d} {:20s} {:10.2f}".format(self.id, self.name, self.sal))
+        
---
+++ FilesPython/program_12.py
@@ -0,0 +1,22 @@
+#A python program to pickle Emp class object
+#pickle - store Emp class object into emp.dat file
+import program_11 as Emp
+import pickle
+ 
+#Open emp.dat file as a binary file for writing
+f = open('emp.dat', 'wb')
+n = int(input('How many employes?'))
+
+for i in range(n):
+    emp_id = input('Enter Emp Id :: ')
+    empName = input('Enter Emp Name :: ')
+    emp_sal = input('Enter Emp Salary :: ')
+
+#Create Emp class object.
+e = Emp.Emp(emp_id,empName,emp_sal)
+
+#store the object into the file f
+pickle.dump(e, f)
+
+#close file
+f.close()

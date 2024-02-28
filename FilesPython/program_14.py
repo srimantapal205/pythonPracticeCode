4f26f3528de547c663d5317facd8cfbff879f32c
Srimanta Pal <srimantapal205@hotmail.com>
2024-02-28 23:49:35 +0530

    #create cities.bin file with cites name

---
+++ FilesPython/program_14.py
@@ -0,0 +1,23 @@
+#A python program to create a bynary file  file and store a few record
+#create cities.bin file with cites name
+#take the size as 20 bytes
+reclen = 20
+
+#open the file in wb mode as binary file
+with open('cites.bin', 'wb') as f:
+    #write data in to the file
+    n = int(input('How many entrise ? :: '))
+    
+    for i in range(n):
+        city = input('Enter city name :: ')
+        #find the length of the city
+        ln = len(city)
+        
+        #increase city name to 20 chars
+        #by adding remaing space
+        city = city + (reclen -ln)*''
+        #convert city name in to byte string
+        city = city.encode()
+        #write the city name into the filr
+        f.write(city)
+

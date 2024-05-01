1776475204fc96268655276f2a7336126849a684
Srimanta Pal <srimantapal205@hotmail.com>
2024-05-01 23:27:40 +0530

    program execute...

---
+++ python_ds_algo/1/illustrative_program/E-1.1.py
@@ -0,0 +1,14 @@
+# Write a program to prompt a user to earn his/her name mailing address and display the enterd information on the screen.
+name = input('Enter you Namr :: ')
+address1 = input('Enter address line 1 : ')
+address2 = input('Enter address line 2 : ')
+address3 = input('Enter address line 3 : ')
+city = input('Enter city name : ')
+state = input('Enter state name: ')
+pin = input('Enter pin : ')
+
+print('Name :: ', name)
+print('Address :: ', address1, '\n',address2, '\n', address3)
+print('City :: ', city)
+print('State :: ', state)
+print('Pin :: ', pin)
